11a03d9104f4824128f92ff4751954aac5fe9591
driica <dricagithub@gmail.com>
2022-10-11 17:36:43 -0300

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,44 @@
+import react ,{useState}from "react";
+import Card from "./Card";
+import axios from "axios";
+const Main=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>Você pode criar qualquer coisa:<br/> basta escrever.</h1>
+                </div>
+                <div className="autor">
+                    <h3> – C. S. Lewis</h3>
+                </div>
+                <div className="row2">
+                    <h2>Encontre seu livro</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Digite o nome do seu livro"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
+                    <img src="./images/livro.png" alt="" />
+                </div>
+            </div>
+
+            <div className="container">
+              {
+                    <Card book={bookData}/>
+              }  
+            </div>
+        </>
+    )
+}
+export default Main;
